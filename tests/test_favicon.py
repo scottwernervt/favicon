46521b177d63713806df568986d9c4aee81fa65b
Scott Werner <scott.werner.vt@gmail.com>
2018-05-18 16:54:22 -0400

    Move dimensions test into link size attribute test

--- tests/test_favicon.py
+++ tests/test_favicon.py
@@ -46,21 +46,24 @@ def test_link_rel_attribute(m, link):
     assert icon.format == 'ico'
 
 
-def test_link_sizes_attribute(m):
+@pytest.mark.parametrize('link,size', [
+    ('<link rel="icon" href="logo.png" sizes="any">', (0, 0)),
+    ('<link rel="icon" href="logo.png" sizes="16x16">', (16, 16)),
+    ('<link rel="icon" href="logo.png" sizes="24x24+">', (24, 24)),
+    ('<link rel="icon" href="logo.png" sizes="32x32 64x64">', (64, 64)),
+    ('<link rel="icon" href="logo.png" sizes="64x64 32x32">', (64, 64)),
+    ('<link rel="icon" href="logo-128x128.png" sizes="any">', (128, 128)),
+])
+def test_link_sizes_attribute(m, link, size):
     m.head('http://mock.com/favicon.ico', text='Not Found', status_code=404)
-    m.get('http://mock.com/',
-          text='<link rel="icon" '
-               'sizes="16x16 32x32" '
-               'type="image/png"'
-               'href="http://mock.com/favicon.png">')
+    m.get('http://mock.com/', text=link)
 
     icons = favicon.get('http://mock.com/')
     assert icons
 
     icon = icons[0]
-    assert icon.url == 'http://mock.com/favicon.png'
-    assert icon.width == 32 and icon.height == 32
-    assert icon.format == 'png'
+    width, height = size
+    assert icon.width == width and icon.height == height
 
 
 def test_link_apple_touch(m):
@@ -86,14 +89,3 @@ def test_link_apple_touch(m):
 def test_is_absolute(url, expected):
     assert is_absolute(url) == expected
 
-
-@pytest.mark.parametrize('link,size', [
-    (s.new_tag('link', href='logo.png', sizes='any'), (0, 0)),
-    (s.new_tag('link', href='logo.png', sizes='16x16'), (16, 16)),
-    (s.new_tag('link', href='logo.png', sizes='16x16+'), (16, 16)),
-    (s.new_tag('link', href='logo.png', sizes='16x16 32x32'), (32, 32)),
-    (s.new_tag('link', href='logo.png', sizes='32x32 16x16'), (32, 32)),
-    (s.new_tag('link', href='logo-64x64.png', sizes='any'), (64, 64)),
-], ids=lambda l: str(l['sizes']) if not isinstance(l, tuple) else None)
-def test_dimensions(link, size):
-    assert dimensions(link) == size
