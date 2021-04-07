56a149e914016b7a7eed726292302b2ad2bee005
Scott Werner <scott.werner.vt@gmail.com>
2021-04-07 16:06:12 -0400

    Add latest python version classifiers.

--- setup.py
+++ setup.py
@@ -87,6 +87,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Utilities",
         "Topic :: Internet :: WWW/HTTP",
